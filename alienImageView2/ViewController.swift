fbe0b08a9e61a299c5ea45bb951385fd6efb48f2
D7703_25 <d7703_25@D7703-25ui-iMac.local>
2019-03-28 15:30:38 +0900

    수정본

--- alienImageView2/ViewController.swift
+++ alienImageView2/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var monsterImage: UIImageView!
+    
+    @IBOutlet var CountLabel: UILabel!
+    
+    var count = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        monsterImage.image = UIImage(named:"frame1.png")
+        //CountLabel.text = String(count)
+        CountLabel.text = "frame\(count).png"
     }
+    
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnAction(_ sender: Any) {
+        
+        count+=1
+        if(count>5){
+            count = 1
+        }
+        monsterImage.image = UIImage(named:"frame\(count).png")
+        CountLabel.text = "frame\(count).png"
     }
-
-
 }
+
+
+
 
